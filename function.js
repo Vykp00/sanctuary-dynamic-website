d454afdc42eada3c2a26b25179feb5a6fa011626
VyKP <88905171+Vykp00@users.noreply.github.com>
2022-08-27 18:40:36 +0300

    Debugging
    Change title name
    + fix id ame

---
+++ function.js
@@ -0,0 +1,26 @@
+console.log("Hello World");
+    var x= 5;
+
+    switch (x):{
+        case 1:{
+            console.log("One")
+            switch(string){
+                case "Yes":{
+                    console.log("Yeah")
+                }
+                case "No":{
+                    console.log("Bummer");
+                }
+            }
+            break;
+        }
+        case 2:{
+            console.log("Two")
+            break;
+        }
+        default:{
+            console.log("None")
+            break;
+        }
+
+    }
